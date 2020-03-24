bec5af3247c85f92a195db2a9e3864d0f0497201
sxdxlxx <352669540@qq.com>
2020-03-24 12:02:35 +0800

    init repo

---
+++ app/src/main/java/com/example/plugin_sad/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.plugin_sad
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
