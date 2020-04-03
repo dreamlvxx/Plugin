cc341e6353b8702c9c61ac71602fb0752ab586e8
sxdxlxx <352669540@qq.com>
2020-04-03 09:17:24 +0800

    asm

---
+++ myplugin/src/main/java/com/asm/LifecycleClassVisitor.java
@@ -0,0 +1,43 @@
+package com.asm;
+
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class LifecycleClassVisitor extends ClassVisitor implements Opcodes {
+
+    private String mClassName;
+
+    public LifecycleClassVisitor(ClassVisitor cv) {
+        super(Opcodes.ASM5, cv);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        //System.out.println("LifecycleClassVisitor : visit -----> started ：" + name);
+        this.mClassName = name;
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        //System.out.println("LifecycleClassVisitor : visitMethod : " + name);
+        MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+        //匹配FragmentActivity
+        if ("android/support/v4/app/FragmentActivity".equals(this.mClassName)) {
+            if ("onCreate".equals(name) ) {
+                //处理onCreate
+                System.out.println("LifecycleClassVisitor : change method ----> " + name);
+                return new LifecycleOnCreateMethodVisitor(mv);
+            }
+        }
+        return mv;
+    }
+
+    @Override
+    public void visitEnd() {
+        //System.out.println("LifecycleClassVisitor : visit -----> end");
+        super.visitEnd();
+    }
+}
